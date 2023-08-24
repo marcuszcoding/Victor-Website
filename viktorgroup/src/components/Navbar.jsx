fcfc80ef4d3d140de009cb461f0c312f5068c48b
marcuszcoding <marcuszcoding@gmail.com>
2023-08-24 16:28:35 -0600

    add basic navbar and scss

--- viktorgroup/src/components/Navbar.jsx
+++ viktorgroup/src/components/Navbar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import styles from '../styles/Navbar.scss';
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <nav className={styles.navbar}>
+      <ul className={styles.navList}>
+        <li className={styles.navItem}><a href="#">Home</a></li>
+        <li className={styles.navItem}><a href="#">About</a></li>
+        <li className={styles.navItem}><a href="#">Services</a></li>
+        <li className={styles.navItem}><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
