d2e15231873fa2002a8470d9c01dc64b3503928c
marcuszcoding <marcuszcoding@gmail.com>
2023-08-25 12:39:52 -0600

    render with react-router-dom

--- victorgroup/src/App.js
+++ victorgroup/src/App.js
@@ -1,14 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Router and related components
 import Navbar from './components/Navbar';
-import Home from './components/Home'
+import Home from './components/Home';
 import './styles/App.scss';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <div>
-        <Home />
-      </div>
+      <Router>
+        <Navbar />
+        <div className='Content'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
