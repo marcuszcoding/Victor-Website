320c9bec92a33b9e4412258b3eae78c03cf3c461
marcuszcoding <marcuszcoding@gmail.com>
2023-08-25 12:24:40 -0600

    testing home component

--- victorgroup/src/App.js
+++ victorgroup/src/App.js
@@ -1,10 +1,14 @@
 import Navbar from './components/Navbar';
+import Home from './components/Home'
 import './styles/App.scss';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <div>
+        <Home />
+      </div>
     </div>
   );
 }
