77e46d74be100a856477fa65bcf6cbe7e5af2493
marcuszcoding <marcuszcoding@gmail.com>
2023-08-25 13:08:37 -0600

    render example home

--- victorgroup/src/components/Home.jsx
+++ victorgroup/src/components/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import '../styles/Home.scss'
 
 function Home() {
   return (
-    <div>HEllO WORLD WHATS UP</div>
-  )
+    <div className="home-container">
+      <h1>Welcome to My Website</h1>
+      <p>This is the home page of my awesome website. Feel free to explore and learn more about what I have to offer.</p>
+      <button className="cta-button">Learn More</button>
+    </div>
+  );
 }
+
 
 export default Home
