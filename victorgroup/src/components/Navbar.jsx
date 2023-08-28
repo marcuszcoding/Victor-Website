936238de16ceb9f92d04235c7f3fdcfe66e95617
marcuszcoding <marcuszcoding@gmail.com>
2023-08-28 11:00:10 -0600

    add navbar logo

--- victorgroup/src/components/Navbar.jsx
+++ victorgroup/src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../styles/Navbar.scss';
+import logo from '../assets/VGHighResolutionLogo.jpg'
 
 function Navbar() {
   return (
     <nav className="navbar">
       <ul className="navList">
+        <img src={logo} alt='Company Logo' className='navbar-logo' />
         <li className="navItem"><a href="#Home" className="navLink">Home</a></li>
         <li className="navItem"><a href="#About" className="navLink">About</a></li>
         <li className="navItem"><a href="#Services" className="navLink">Services</a></li>
